af7f122956d2308bde01754016da331b133e72d4
Evan Light <evan@tripledogdare.net>
2014-02-09 17:02:27 -0500

    Oops! Clearly forgot to fix that when lifted from seanmiller!

--- init.el
+++ init.el
@@ -84,11 +84,10 @@
 
 ;; -- coding -----------------------------------------------------------------
 (setq tags-table-list
-  '("/Users/sean/projects/qs-core/TAGS"))
+  '("./TAGS"))
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
-
 (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
